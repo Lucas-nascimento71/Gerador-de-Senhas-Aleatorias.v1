fc62f9534eee76f258c1e219c8b7cb3b389f6a86
Lucas-nascimento71 <lnfsouza@hotmail.com>
2024-10-08 12:42:04 -0300

    v2

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import random
 import string
 
 print('=-=-=-==-=--=-=-==-=GERADOR DE SENHAS ALEATÓRIAS=-=-=-=-=--=-=-=-=-=-=-=-=-=')
+print('Características:', '\n', '12 caracteres', '\n', 'Alfanuméricos', '\n', 'Letras Maiúsculos', '\n', 'Letras minúsculas', '\n', 'Caracteres Especiais')
+print('=-=-=-==-=--=-=-==-==-=-=-=-=--=-=-=-=-=-=-=-=-==-=-=-=-=-=-=-=---==-=-=-==')
 
 def gerar_senhas(tamanho=12):
     caracteres = string.ascii_letters + string.punctuation
